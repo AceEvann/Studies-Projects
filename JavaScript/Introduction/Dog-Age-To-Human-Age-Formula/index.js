f3053f2160f60a979d81a716debbb6f729380d55
Ace <102817244+AceEvann@users.noreply.github.com>
2024-08-21 10:47:19 -0300

    Create index.js

---
+++ JavaScript/Introduction/Dog-Age-To-Human-Age-Formula/index.js
@@ -0,0 +1,7 @@
+var dogAge = prompt("Insert your dog's age to descover how much it is in human age: ");
+
+// Dog age calculation to human age
+var humanAge = ((dogAge - 2) * 4) + 21;
+
+// Inform the age to the user
+alert("Your dog is " + humanAge + " years old in human years!");
