447aa94303af65cefc6181b64647fb0a3dd07322
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-13 16:06:57 -0300

    Update index.js

--- JavaScript/Introduction/Milk-Bottle-Robot/index.js
+++ JavaScript/Introduction/Milk-Bottle-Robot/index.js
@@ -1,3 +1,4 @@
+// Main function
 function getMilk(money, costPerBottle) {
     console.log("leaveHouse");
     console.log("moveRight");
@@ -8,6 +9,7 @@ function getMilk(money, costPerBottle) {
     console.log("moveUP");
     console.log("moveRight");
     console.log("moveRight");
+    //Console with the initial request
     console.log("buy " + calcBottles(money, costPerBottle) + " bottles of milk.");
     console.log("moveLeft");
     console.log("moveLeft");
@@ -19,21 +21,29 @@ function getMilk(money, costPerBottle) {
     console.log("moveLeft");
     console.log("entereHouse");
 
-    // Reminder or the division
+    // Return the calcChange function
     return calcChange(money, costPerBottle);
 }
 
+// Function to know how many bottles could be bought
 function calcBottles(startingMoney, costPerBottle) {
 
+    // Variable that do the division
     var numberOfBottles = Math.floor(startingMoney / costPerBottle);
 
+    // Return the variable
     return numberOfBottles;
 }
 
+// Function that calculates the change
 function calcChange(startingAmount, costPerBottle) {
+
+    // Variable that take the rest of the division
     var change = startingAmount % costPerBottle;
+
+     // Return the variable
     return change;
 }
 
-
+// Final console
 console.log("Hello sir, here is your " + getMilk(5, 3) + "$ of change.");
