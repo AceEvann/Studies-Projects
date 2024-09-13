7a4386c6112ffff3b85c6303bcd86475bf08aca9
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-13 11:20:46 -0300

    Update index.js

--- JavaScript/Introduction/Life-in-Weeks-Coding-Exercise/index.js
+++ JavaScript/Introduction/Life-in-Weeks-Coding-Exercise/index.js
@@ -1,19 +1,19 @@
 function lifeInWeeks(age) {   
         
-        // Subtract age by 90 
-        var yarsRemaining = 90 - age;
+        // Subtract 90 by the age 
+        var yearsRemaining = 90 - age;
 
         // Count the days
-        var days = yarsRemaining * 365;
+        var days = yearsRemaining * 365;
 
         // Count the weeks
-        var weeks = yarsRemaining * 52;
+        var weeks = yearsRemaining * 52;
 
         // Count the months
-        var months = yarsRemaining * 12;
+        var months = yearsRemaining * 12;
         
         // Console with the asked message
-        cconsole.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+        console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
     }
     
 lifeInWeeks(56)
