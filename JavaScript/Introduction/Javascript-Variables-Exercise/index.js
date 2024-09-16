4187fe7077cc9891d32602d14da05b2ace4c5d76
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-16 13:06:35 -0300

    Update index.js

--- JavaScript/Introduction/Javascript-Variables-Exercise/index.js
+++ JavaScript/Introduction/Javascript-Variables-Exercise/index.js
@@ -3,9 +3,13 @@ function test() {
     var b = "8";
     
 /***********Do not change the code above 👆*******/
-//Write your code on lines 7 - 9:
+    // Create a variable to keep the value of a
     var c = a;
+
+    // Change the value of a to b
     a = b;
+
+    // Change the value of b to the old value of a
     b = c;
 /***********Do not change the code below 👇*******/
 
